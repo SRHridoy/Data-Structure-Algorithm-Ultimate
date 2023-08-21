177cb8420d3a0b5ea8cc71578620788ca5abe012
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-08-21 23:06:59 +0600

    Completed Array Data Structures...

---
+++ DSA  in C Full/03_2D Arrays/01_compile_time_initialization.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    //Row 2 and column 3
+    int a[2][3] ={  1, 2, 3,
+                    4, 5, 6
+                };
+
+    int b[2][3] = {
+                    {1, 2, 3},
+                    {4, 5, 6}
+                };
+    int c[][3] = {1,2,3};//Valid
+
+//Optional to give Row size but column must...
+
+    //int d[3][] = {2, 4, 5};//Invalid
+}
