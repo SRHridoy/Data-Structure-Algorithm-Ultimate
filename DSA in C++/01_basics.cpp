43d7d4cba32026c54f21ef24cd9139cb1079ce86
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-11-08 20:25:51 +0600

    Started...

---
+++ DSA in C++/01_basics.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    int a, b;
+    // cin >> a;//>> Extraction operator...
+    // cin >> b;
+    cout << "Enter Two Numbers : ";
+    cin >> a >> b;
+    cout << "The sum is : ";
+    cout << a + b;//<< Insertion operator...
+    return 0;
+}
