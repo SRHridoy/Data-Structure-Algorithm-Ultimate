54877bb3ad1c1637610cd6f4a487b8b507e3cb3c
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-11-08 21:27:14 +0600

    Started DSA in C++ on 8 Nov, Seriourly and revise the topics...

---
+++ DSA in C++/01_Basic_C++/04_If Else Statement in c++ || Loop in C++/01_basics.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    int a, b;
+    // cin >> a;//>> Extraction operator...
+    // cin >> b;
+    cout << "Enter Two Numbers : ";
+    cin >> a >> b;
+    cout << "The sum is : ";
+    cout << a + b;//<< Insertion operator...
+
+    //DATA TYPES : 
+    char c = 'a';
+    cout << c << endl;
+
+    float f = 2.32;
+    cout << f << endl;
+    
+    bool b = true;
+    cout << b << endl;
+    return 0;
+}
